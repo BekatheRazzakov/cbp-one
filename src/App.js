616d0105abc0c36d0cb10fd19e0118e0d4a8274b
Bektur Razzakov <brzzkv@gmail.com>
2024-10-22 18:03:09 +0600

    install tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}", // Specify the directories with your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
